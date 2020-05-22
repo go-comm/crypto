516d7ad0e2a1a89c91e452cd4c1c541d81f30ef6
xwzhou <xwzhou@yeah.net>
2020-05-22 11:23:54 +0800

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/go-comm/crypto
 
 go 1.14
 
-require github.com/go-comm/xbytes v0.0.0-20200428054400-bfff8f4f80c5
+require github.com/go-comm/xbytes v0.0.0-20200522025743-68ffeb853680
--- xbase64/base64.go
+++ xbase64/base64.go
@@ -13,45 +13,63 @@ var (
 	Base64URL = NewBase64Stream(base64.URLEncoding)
 )
 
-func NewBase64Stream(enc *base64.Encoding) *base64Stream {
-	return &base64Stream{enc: enc}
+func NewBase64Stream(enc *base64.Encoding) *Base64Stream {
+	return &Base64Stream{enc}
 }
 
-type base64Stream struct {
-	enc *base64.Encoding
+type Base64Stream struct {
+	Encoding *base64.Encoding
 }
 
-func (e *base64Stream) EncodeFromReader(w io.Writer, r io.Reader) error {
-	ew := base64.NewEncoder(e.enc, w)
+func (e *Base64Stream) EncodeFromReader(w io.Writer, r io.Reader) error {
+	ew := base64.NewEncoder(e.Encoding, w)
 	xbytes.ZeroCopy(ew, r)
 	return ew.Close()
 }
 
-func (e *base64Stream) Encode(w io.Writer, data []byte) error {
+func (e *Base64Stream) Encode(w io.Writer, data []byte) error {
 	if buf, ok := w.(*bytes.Buffer); ok {
-		buf.Grow(e.enc.EncodedLen(len(data)))
+		buf.Grow(e.Encoding.EncodedLen(len(data)))
 	}
-	ew := base64.NewEncoder(e.enc, w)
+	ew := base64.NewEncoder(e.Encoding, w)
 	ew.Write(data)
 	return ew.Close()
 }
 
-func (e *base64Stream) DecodeFromReader(w io.Writer, r io.Reader) error {
-	er := base64.NewDecoder(e.enc, r)
+func (e *Base64Stream) Marshal(dst []byte, src []byte) ([]byte, error) {
+	encodedLen := e.Encoding.EncodedLen(len(src))
+	if len(dst) < encodedLen {
+		dst = make([]byte, encodedLen)
+	}
+	e.Encoding.Encode(dst, src)
+	return dst[:encodedLen], nil
+}
+
+func (e *Base64Stream) DecodeFromReader(w io.Writer, r io.Reader) error {
+	er := base64.NewDecoder(e.Encoding, r)
 	_, err := xbytes.ZeroCopy(w, er)
 	return err
 }
 
-func (e *base64Stream) Decode(w io.Writer, data []byte) error {
+func (e *Base64Stream) Decode(w io.Writer, data []byte) error {
 	if buf, ok := w.(*bytes.Buffer); ok {
-		buf.Grow(e.enc.DecodedLen(len(data)))
+		buf.Grow(e.Encoding.DecodedLen(len(data)))
 	}
 	return e.DecodeFromReader(w, bytes.NewReader(data))
 }
 
-func (e *base64Stream) DecodeToBytes(s *string) ([]byte, error) {
+func (e *Base64Stream) DecodeToBytes(s *string) ([]byte, error) {
 	buf := xbytes.StringToBytes(s)
-	dbuf := make([]byte, e.enc.DecodedLen(len(buf)))
-	n, err := e.enc.Decode(dbuf, buf)
+	dbuf := make([]byte, e.Encoding.DecodedLen(len(buf)))
+	n, err := e.Encoding.Decode(dbuf, buf)
 	return dbuf[:n], err
+}
+
+func (e *Base64Stream) Unmarshal(dst []byte, src []byte) ([]byte, error) {
+	decodedLen := e.Encoding.DecodedLen(len(src))
+	if len(dst) < decodedLen {
+		dst = make([]byte, decodedLen)
+	}
+	n, err := e.Encoding.Decode(dst, src)
+	return dst[:n], err
 }
